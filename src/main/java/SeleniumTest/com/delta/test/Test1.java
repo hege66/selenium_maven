a0ad2497c309b1b0f22945feec7d2dd00cba277f
hege <18839418083@163.com>
2018-09-24 19:18:27 +0800

    update pom.xml

--- src/main/java/SeleniumTest/com/delta/test/Test1.java
+++ src/main/java/SeleniumTest/com/delta/test/Test1.java
@@ -1,9 +1,7 @@
 package SeleniumTest.com.delta.test;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
